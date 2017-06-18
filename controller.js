230fb06dbd11baa16129b8b64d83df0c6fb21b61
jsr <jsr@jsrs-MacBook-Pro.local>
2017-06-18 12:33:57 -0500

    angular

---
+++ controller.js
@@ -0,0 +1,11 @@
+﻿var linkrControllers = angular.module('myControllers', []);
+
+linkrControllers.controller('MyCtrl', function($scope) {
+  $scope.basketballPlayer = {
+    name: 'Johnny Basketball',
+    number: '33',
+    position: 'Guard',
+    points: 0,
+    assists: 0
+  };
+});
---
+++ directive.js
@@ -0,0 +1,11 @@
+﻿var myDirectives = angular.module('myDirectives', []);
+
+myDirectives.directive('basketballPlayer', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      player: '='
+    },
+    templateUrl: 'bball-player-template.html'
+  };
+});
